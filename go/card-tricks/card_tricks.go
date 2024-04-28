5d661f3da740e1cedb52225a2e12cd7fc31937ca
Brandon Card <bozo3887@gmail.com>
2024-04-28 00:22:26 -0400

    solved card-tricks

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -8,21 +8,36 @@ func FavoriteCards() []int {
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
-	panic("Please implement the GetItem function")
+	if index >= 0 && index < len(slice) { // checks to see if index is in bounds
+		return slice[index]
+	}
+	return -1
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-	panic("Please implement the SetItem function")
+	if index >= 0 && index < len(slice) {
+		slice[index] = value
+	} else {
+		slice = append(slice, value)
+	}
+	return slice
 }
 
 // PrependItems adds an arbitrary number of values at the front of a slice.
 func PrependItems(slice []int, values ...int) []int {
-	panic("Please implement the PrependItems function")
+	if len(values) == 0 {
+		return slice
+	}
+	tempSlice := append(values, slice...)
+	return tempSlice
 }
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	panic("Please implement the RemoveItem function")
+	if index >= 0 && index < len(slice) {
+		return append(slice[:index], slice[index+1:]...)
+	}
+	return slice
 }
