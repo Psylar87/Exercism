f33f2f303010047761ff1b6e46158c0176d24900
Brandon Card <bozo3887@gmail.com>
2024-05-02 00:37:28 -0400

    Implemented functions

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,18 +1,26 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, TimePerLayer int) int {
+	panic("")
+}
 
-// TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	panic("")
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendsIngredients []string, myIngredients []string) {
+	panic("")
+}
 
-// TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	panic("")
+}
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
 // The function body only needs to contain `panic("")`.
-// 
+//
 // This will make the tests compile, but they will fail.
 // You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
+// an increasing number of tests passing as you implement more
 // functionality.
