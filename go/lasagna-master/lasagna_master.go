6e03a35c047e5f1d31ae1f748781105ac4d42671
Brandon Card <bozo3887@gmail.com>
2024-05-05 14:24:45 -0400

    solved-lasagna_master

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,29 +1,37 @@
 package lasagna
 
 func PreparationTime(layers []string, TimePerLayer int) int {
-	if len(layers) == 0 {
-		return 0
+	if TimePerLayer == 0 {
+		return len(layers) * 2
 	}
 	return len(layers) * TimePerLayer
 }
 
-func Quantities(layers []string) (int, float64) {
-	panic("")
+func Quantities(layers []string) (noodles int, sauce float64) {
+	gramCount := 0
+	litersCount := 0
+	for _, layer := range layers {
+		switch layer {
+		case "noodles":
+			gramCount++
+			noodles = gramCount * 50
+		case "sauce":
+			litersCount++
+			sauce = float64(litersCount) * 0.2
+		}
+	}
+	return noodles, sauce
 }
 
 func AddSecretIngredient(friendsIngredients []string, myIngredients []string) {
-	panic("")
+	if len(friendsIngredients) > 0 && len(myIngredients) > 0 {
+		(myIngredients)[len(myIngredients)-1] = (friendsIngredients)[len(friendsIngredients)-1]
+	}
 }
 
-func ScaleRecipe(quantities []float64, portions int) []float64 {
-	panic("")
+func ScaleRecipe(quantities []float64, portions int) (scaled []float64) {
+	for _, portion := range quantities {
+		scaled = append(scaled, float64(portions)/2.0*portion)
+	}
+	return scaled
 }
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
