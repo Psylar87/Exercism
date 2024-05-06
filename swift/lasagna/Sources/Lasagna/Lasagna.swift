77641152794b3b40f57c307c1a3dd4d2b7008576
Brandon Card <bozo3887@gmail.com>
2024-05-05 20:32:32 -0400

    solved swift lesson & added lesson

--- swift/lasagna/Sources/Lasagna/Lasagna.swift
+++ swift/lasagna/Sources/Lasagna/Lasagna.swift
@@ -1,7 +1,15 @@
-// TODO: define the 'expectedMinutesInOven' constant
 
-// TODO: define the 'remainingMinutesInOven' function
+let expectedMinutesInOven = 40
 
-// TODO: define the 'preparationTimeInMinutes' function
+func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
+    return expectedMinutesInOven - elapsedMinutes
+}
 
-// TODO: define the 'totalTimeInMinutes' function
+func preparationTimeInMinutes(layers: Int) -> Int {
+    return layers * 2
+}
+
+func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
+    let preparationTime = preparationTimeInMinutes(layers: layers)
+    return preparationTime + elapsedMinutes
+}
