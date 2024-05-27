b00d6957e231f9e7f20343c2cf73be5b1009300f
Brandon Card <bozo3887@gmail.com>
2024-05-27 18:06:01 -0400

    solved lesson

--- go/interest-is-interesting/interest_is_interesting.go
+++ go/interest-is-interesting/interest_is_interesting.go
@@ -2,20 +2,35 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+	if balance < 0 {
+		return 3.213
+	} else if balance >= 0 && balance < 1000 {
+		return 0.5
+	} else if balance >= 1000 && balance < 5000 {
+		return 1.621
+	} else if balance >= 5000 {
+		return 2.475
+	}
+	return 0.0
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	return balance * float64(InterestRate(balance)) / 100
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return Interest(balance) + balance
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	years := 0
+
+	for balance < targetBalance {
+		balance = AnnualBalanceUpdate(balance)
+		years++
+	}
+	return years
 }
